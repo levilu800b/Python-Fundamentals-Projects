7288967dd6a38a71768d26a48ba2d92605c9da3e
levilu800b <levilu800b@gmail.com>
2022-12-15 14:11:05 +0000

    project start fix

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -34,12 +34,12 @@ game_images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
 if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number, you lose!")
+  print("You typed an invalid number, you lose!")
 else:
     print(game_images[user_choice])
 
     computer_choice = random.randint(0, 2)
-    print("Computer chose: ")
+    print("Computer chose:")
     print(game_images[computer_choice])
 
     if user_choice == 0 and computer_choice == 2:
