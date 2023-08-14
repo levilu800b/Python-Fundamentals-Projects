bce5923dd2965e2bd50f8bb871cb527398b7ef96
levilu800b <levilu800b@gmail.com>
2023-08-14 16:26:30 +0100

    save issue

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,29 +1,29 @@
-rock = '''
+rock = """
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-'''
+"""
 
-paper = '''
+paper = """
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-'''
+"""
 
-scissors = '''
+scissors = """
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-'''
+"""
 
 # Write your code below this line 👇
 
@@ -34,7 +34,7 @@ game_images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
 if user_choice >= 3 or user_choice < 0:
-  print("You typed an invalid number, you lose!")
+    print("You typed an invalid number, you lose!")
 else:
     print(game_images[user_choice])
 
--- Treasure Island/main.py
+++ Treasure Island/main.py
@@ -24,7 +24,8 @@
 # You can also add your own ASCII art. Just remember to add three single quotes ''' at the start and at the end of
 # your artwork to turn it into a multi-line string.
 
-print('''
+print(
+    '''
 *******************************************************************************
           |                   |                  |                     |
  _________|________________.=""_;=.______________|_____________________|_______
@@ -45,7 +46,8 @@ ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
-''')
+'''
+)
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
@@ -55,11 +57,15 @@ print("Your mission is to find the treasure.")
 
 choice1 = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right" ').lower()
 if choice1 == "left":
-    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a '
-                    'boat. Type "swim" to swim across. ').lower()
+    choice2 = input(
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a '
+        'boat. Type "swim" to swim across. '
+    ).lower()
     if choice2 == "wait":
-        choice3 = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and '
-                        'one blue. Which colour do you choose? ').lower()
+        choice3 = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and "
+            "one blue. Which colour do you choose? "
+        ).lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over.")
         elif choice3 == "yellow":
